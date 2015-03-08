b2daa5bd48dbb805d5300b257c77bed5898e8308
Joshua S. <joshuarecife@gmail.com>
2015-03-07 22:52:00 -0300

    Exercícios básicos em Java.

---
+++ ExerciciosJava/src/com/joshua/exer01/ExercicioJava01.java
@@ -0,0 +1,11 @@
+package com.joshua.exer01;
+
+public class ExercicioJava01 {
+
+	public static void main(String[] args) {
+		//1) Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+		
+		System.out.println("Alo Mundo!");
+	}
+
+}
